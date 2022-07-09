e8c9480b42bf55ba8af44ed0f68405bce052f6cf
un-boxing-man <unboxingman@hotmail.com>
2022-07-09 16:52:54 -0700

    add stafflist

--- index.d.ts
+++ index.d.ts
@@ -1,18 +1,30 @@
 declare module 'boxing-hosting-utils' {
 
 
-    // interface categorys {
-    //    Utility: "Utility";
-    // }
 
 
     interface getIDList {
         error: boolean;
         message: string;
     }
+    interface getStaffList {
+        error: boolean;
+        message: string;
+    }
+    interface postServers {
+        error: boolean;
+        message: string;
+    }
 
 
     export default class Client {
-        public getIDLiss(): Promise<getIDList>;
+        constructor(DStoken: String, toptoken: String, boatstoken: String);
+
+        public DStoken: String;
+        public toptoken: String;
+        public boatstoken: String
+        public getIDList(): Promise<getIDList>;
+        public getStaffList(): Promise<getStaffList>;
+        public postServers(clientID: string, serverCount: number): Promise<postServers>;
     }
 }
--- index.js
+++ index.js
@@ -4,9 +4,16 @@
  * @license MIT
  */
 const wump = require('wumpfetch');
-const parser = require(`body-parser`)
+//const parser = require(`body-parser`)
 
 module.exports = class Client {
+
+    constructor(DStoken, toptoken, ) {
+        this.DStoken = DStoken;
+        this.toptoken = toptoken;
+
+
+    }
 
     /**
      * get idlist.
@@ -24,6 +31,61 @@ module.exports = class Client {
             } catch (err) {
                 reject(new Error(err));
             }
+        });
+    }
+    /**
+     * get Stafflist.
+     * 
+     */
+    async getStaffList() {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const res = await wump(`https://bots.unboxingman.com/util/StaffList.json`, {
+                    method: 'GET'
+                }).send();
+                var json = await res.json()
+
+                resolve(json);
+            } catch (err) {
+                reject(new Error(err));
+            }
+        });
+    }
+
+    async postServers(clientID, servers) {
+        if (typeof servers !== 'number') throw new TypeError('Server count must be a number');
+        if (typeof clientID !== 'string') throw new TypeError('Bot ID must be a string');
+        var Top = "";
+        var ds = "";
+        if (this.DStoken) ds = this.DStoken;
+        if (this.toptoken) Top = this.toptoken;
+        console.log(ds, Top);
+        return new Promise(async (resolve, reject) => {
+            try {
+                const res = await wump(`https://bots.unboxingman.com/api/post/servers?id=${clientID}`, {
+                    method: 'POST',
+                    data: {
+                        'Servers': servers,
+                        "DSToken": ds.toString(),
+                        "TopToken": top
+                    }
+                }).send();
+                var json = await res.json()
+                var ds = ""
+                var top = ""
+                var status = json.status;
+                ds = json.ds
+                top = json.top
+                var send = ds + top + status
+                console.log(json);
+                // console.log(res);
+                console.log(send);
+                resolve(send);
+            } catch (err) {
+                console.log(err)
+                reject(new Error(err));
+            }
+
         });
     }
 
